1ff5a327befbbd29cdc0b6505113bf8dd1423cfa
revitt17 <kirill.shkirov@outlook.com>
2022-09-07 21:24:28 +0200

    selenium

--- project/main.py
+++ project/main.py
@@ -1,34 +1,54 @@
 from notification import WebPushNotification
 from pybot import PyBot
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from Secrets import SECRETS
+import time
+
+LOGFILE_PATH = f"/home/{SECRETS.USER}/.config/google-chrome/Default/"\
+    "Platform Notifications/000003.log"
 
 def main():
 
-    # Telegram_bot
+    web_push_not = WebPushNotification(LOGFILE_PATH)
     pybot = PyBot(SECRETS.BOT_TOKEN, SECRETS.CHANNEL)
 
-    # Notification
-    logfile_path = f"/home/{SECRETS.USER}/.config/google-chrome/Default/"\
-        "Platform Notifications/000003.log"
-
-    web_push_not = WebPushNotification(logfile_path)
-
     # Add free_bets to dict
+
     free_bets = {}
     free_bets = free_bets | web_push_not.read()
 
     # For each item in dict send a msg to telegram_channel
+
     for key, value in free_bets.items():
         pybot.send_msg(key, value)
 
     # Take a free bets with selenium
-    pass
 
+    driver = webdriver.Chrome(executable_path="web_driver/chromedriver")
+
+    is_first = 1
+    
+    for key, value in free_bets.items():
+        driver.get(key)
+        if is_first:
+            driver.find_element(
+                "id", "field_email").send_keys(SECRETS.BIDOO_USER)
+            driver.find_element(
+                "id", "password").send_keys(SECRETS.BIDOO_PASS)
+            js='javascript:document.getElementsByClassName("btlogin btn\
+                btn-grey btn-block btn-lg signup-btn")[1].click();\
+                window.open("/checkout")'
+            driver.execute_script(js)
+            is_first = 0
+            time.sleep(5)
+        time.sleep(5)
+        driver.refresh()
+ 
     # Clear logfile and email
     pass
 
-    #print(free_bets)
+    print(free_bets)
 
 if __name__ == '__main__':
     main()
